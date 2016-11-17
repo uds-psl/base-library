55cdd655c08f36871bc2495b87da7a1ad204c52f
Yannick Forster <forster@ps.uni-saarland.de>
2016-11-17 14:59:47 +0100

    Introduced tactic "lock firstorder", which only works on the hypotheses

--- Tactics.v
+++ Tactics.v
@@ -22,3 +22,11 @@ Tactic Notation "destruct" "*" "eqn" ":" "_" := destruct * eqn:E.
 
 Ltac fstep N := unfold N; fold N.
 
+Inductive Lock : Prop -> Prop := L (P : Prop) (x : P) : Lock P.
+
+Lemma Lock_eq (x : Prop) : Lock x <-> x.
+Proof.
+  firstorder. now destruct H. now econstructor.
+Qed.
+
+Tactic Notation "lock" "firstorder" := eapply Lock_eq; firstorder; match goal with [ |- Lock _] => eapply Lock_eq end.
