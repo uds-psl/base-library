88df779d208493813b367a31cb3ad0cc72f18038
Maximilian Wuttke <mwuttke97@gmail.com>
2018-02-10 18:48:56 +0100

    tactics

--- Tactics/Tactics.v
+++ Tactics/Tactics.v
@@ -24,14 +24,14 @@ Tactic Notation "intros" "***" := repeat (intros ?).
 
 Ltac fstep N := unfold N; fold N.
 
-Inductive Lock : Prop -> Prop := L (P : Prop) (x : P) : Lock P.
+Inductive Lock : Type -> Type := L (P : Type) (x : P) : Lock P.
 
-Lemma do_Lock (x : Prop) : Lock x -> x.
+Lemma do_Lock (x : Type) : Lock x -> x.
 Proof.
-  firstorder. now destruct H. 
+  firstorder. now destruct X.
 Qed.
 
-Lemma do_unLock (x : Prop) : x -> Lock x.
+Lemma do_unLock (x : Type) : x -> Lock x.
 Proof.
   firstorder. now econstructor.
 Qed.
@@ -44,10 +44,45 @@ Tactic Notation "unlock" ident(H) := eapply do_Lock in H.
 
 Tactic Notation "locked" tactic(t) := lock; t; unlock.
 
-Tactic Notation "unlock" "all" := repeat match goal with
-                                         | [ H : Lock _ |- _] => unlock H
-                                         | [ |- Lock _ ] => unlock
-                                        end.
+Tactic Notation "unlock" "all" :=
+  repeat match goal with
+         | [ H : Lock _ |- _] => unlock H
+         | [ |- Lock _ ] => unlock
+         end.
+
+Tactic Notation "is_locked" :=
+  match goal with
+  | [ |- L _ ] => idtac
+  | _ => fail
+  end.
+
+Tactic Notation "is_locked" ident(H) :=
+  match type of H with
+  | (L _ ) => idtac
+  | _ => fail
+  end.
+
+Tactic Notation "is_unlocked" :=
+  match goal with
+  | [ |- L _ ] => fail
+  | _ => idtac
+  end.
+
+Tactic Notation "is_unlocked" ident(H) :=
+  match type of H with
+  | (L _ ) => fail
+  | _ => idtac
+  end.
+
+(* lock all lockable hypothesis, but not the goal *)
+Tactic Notation "lock" "all" :=
+  repeat
+    match goal with
+    | [ H : _ |- _ ] => is_unlocked H; lock H
+    end.
+
+
+
 (* From Program.Tactics *)
 Ltac destruct_one_pair :=
  match goal with
@@ -59,7 +94,7 @@ Ltac destruct_pairs := repeat (destruct_one_pair).
 
 
 
-(* Show the non-dependent hypothesis of a hypothesis that is a implication and specialize it *)
+(* Prove the non-dependent hypothesis of a hypothesis that is a implication and specialize it *)
 Tactic Notation "spec_assert" hyp(H) :=
   let H' := fresh in
   match type of H with
@@ -88,6 +123,23 @@ Tactic Notation "spec_assert" hyp(H) "as" simple_intropattern(p) "by" tactic(T) 
   | ?A -> _ =>
     assert A as H' by T; specialize (H H') as p; clear H'
   end.
+
+
+
+(* Some debug tactics *)
+
+Ltac print_goal :=
+  match goal with
+  | [ |- ?H ] => idtac H
+  end.
+
+Ltac print_goal_cbn :=
+  match goal with
+  | [ |- ?H ] =>
+    let H' := eval cbn in H in idtac H'
+  end.
+
+Ltac print_type e := first [ let x := type of e in idtac x | idtac "Untyped:" e ].
 
 
 Require Export AutoIndTac.
